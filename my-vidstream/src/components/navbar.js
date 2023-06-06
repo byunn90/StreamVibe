a7d9a1740554a857c3b0cd4e23c260acb01cb77d
byunn90 <kayhanturk90@Live.com>
2023-06-06 14:23:23 +1000

    finished searchbar and logo position

--- my-vidstream/src/components/navbar.js
+++ my-vidstream/src/components/navbar.js
@@ -14,26 +14,34 @@ function Navbar() {
   return (
     <>
       <nav>
-        <div className="flex justify-center items-center">
-          <a href="www.google.com">
-            <img className="logo-scope w-12" src={myLogo} alt="Logo" />
-          </a>
-          <form
-            className="typeo-search flex items-center justify-center gap-2"
-            action="/search"
-            method="get"
-          >
-            <input
-              type="text"
-              id="header-search"
-              placeholder="Search.."
-              name="s"
-              className="outline-none rounded-md border-2 border-blue-600"
-            />
-            <button className="flex items-center justify-center focus:outline-none focus:ring rounded-md border-2 rounded-full wd-10 shadow hover:bg-gray-400 hover:text-white">
-              <FontAwesomeIcon icon={faSearch} />
-            </button>
-          </form>
+        <div className="flex items-center">
+          <div>
+            <a className="type-scope-2 gap-2" href="www.google.com">
+              <img
+                className="logo-scope w-12 flex-shrink-0"
+                src={myLogo}
+                alt="Logo"
+              />
+            </a>
+          </div>
+          <div className="flex justify-center flex-grow">
+            <form
+              className="typeo-search m-1 flex items-center gap-2"
+              action="/search"
+              method="get"
+            >
+              <input
+                type="text"
+                id="header-search"
+                placeholder="Search.."
+                name="s"
+                className="outline-none rounded-md border-2 border-blue-600"
+              />
+              <button className="flex items-center justify-center focus:outline-none focus:ring rounded-md border-2 rounded-full wd-10 shadow hover:bg-gray-400 hover:text-white">
+                <FontAwesomeIcon icon={faSearch} />
+              </button>
+            </form>
+          </div>
         </div>
       </nav>
       <div />
