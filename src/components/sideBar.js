909148e38bf0f3918d6612c78a761c4ccd3dc079
byunn90 <kayhanturk90@Live.com>
2023-06-15 16:11:12 +1000

    side creation

---
+++ src/components/sideBar.js
@@ -0,0 +1,12 @@
+function sideBar() {
+  return (
+    <>
+      <div className="side-container">
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </div>
+      ;
+    </>
+  );
+}
